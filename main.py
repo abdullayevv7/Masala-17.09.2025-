8f7ffc28ae0e7ffb67cd568364a60d150af2ad01
Farhod <zxcvbnmqazwsx287@gmail.com>
2025-09-17 15:36:36 +0500

    Yangilik!
    Biz yangilik qilishdan to'xtamaymiz!

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
 ##1-Masala: Foydalanuvchidan son qabul qilib, uning musbat yoki manfiy ekanligini aniqlang va natijani chiqaring.
 
-# son = float(input("Iltimos, biror son kiriting: ")) # foydalanuvchidan son qabul qilish
+son = float(input("Iltimos, biror son kiriting: ")) # foydalanuvchidan son qabul qilish
 
-# if son > 0: # agar son 0 dan katta bo'lsa
-#     print(f"{son} - musbat son") # son musbat ekanligini chiqarish
+if son > 0: # agar son 0 dan katta bo'lsa
+    print(f"{son} - musbat son") # son musbat ekanligini chiqarish
 
-# elif son == 0: # agar son 0 ga teng bo'lsa
-#     print(f"{son} - son nolga teng") # son nolga teng ekanligini chiqarish
+elif son == 0: # agar son 0 ga teng bo'lsa
+    print(f"{son} - son nolga teng") # son nolga teng ekanligini chiqarish
 
-# else: # aks holda
-#     print(f"{son} - manfiy son") # son manfiy ekanligini chiqarish
+else: # aks holda
+    print(f"{son} - manfiy son") # son manfiy ekanligini chiqarish
 
 
 ##2-Masala: Foydalanuvchidan tug'ilgan yilini qabul qilib, uning yoshini hisoblang va natijani chiqarib bering.
